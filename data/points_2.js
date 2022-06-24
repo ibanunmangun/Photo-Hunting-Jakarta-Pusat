a06ae1d7c2ca377b0fbb9aa29f16c261b99b5493
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 22:09:36 +0800

    Add files via upload

---
+++ data/points_2.js
@@ -0,0 +1,11 @@
+var json_points_2 = {
+"type": "FeatureCollection",
+"name": "points_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Lapangan Banteng Park", "desc": "Lapangan Banteng Park, RRHP+Q2F, Pasar Baru, Sawah Besar, Central Jakarta City, Jakarta 10710, Indonesia", "X": "106.835037", "Y": "-6.170555" }, "geometry": { "type": "Point", "coordinates": [ 106.8350378, -6.170555 ] } },
+{ "type": "Feature", "properties": { "name": "Bundaran HI", "desc": "Jl. Kebon Melati 1 No.5, RT.1\/RW.5, Menteng, Kec. Menteng, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10310", "X": "106.823049", "Y": "-6.1950006" }, "geometry": { "type": "Point", "coordinates": [ 106.823049224818476, -6.195000615269493 ] } },
+{ "type": "Feature", "properties": { "name": "Taman Budaya Dukuh Atas", "desc": "Taman Dukuh Atas, Jl. Jenderal Sudirman No.20, RT.10\/RW.20, Kb. Melati, Kecamatan Tanah Abang, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10230, Indonesia", "X": "106.822907", "Y": "-6.2008928" }, "geometry": { "type": "Point", "coordinates": [ 106.8229078, -6.2008928 ] } },
+{ "type": "Feature", "properties": { "name": "Jembatan Pinisi", "desc": "JPO Pinisi, Mayapada Tower 2, RT.4\/RW.2, Karet Kuningan, Setiabudi, South Jakarta City, Jakarta, Indonesia", "X": "106.819624", "Y": "-6.2131267" }, "geometry": { "type": "Point", "coordinates": [ 106.8196241, -6.2131267 ] } }
+]
+}
